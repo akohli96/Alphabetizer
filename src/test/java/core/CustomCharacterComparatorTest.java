2490aff3ab70b0b73f8ea6d943995f1681b3c125
Ayush Kohli <ayushk96@hotmail.com>
2019-04-13 17:51:23 -0500

    Added unit tests for comparator

--- src/test/java/core/CustomCharacterComparatorTest.java
+++ src/test/java/core/CustomCharacterComparatorTest.java
@@ -16,16 +16,30 @@ public class CustomCharacterComparatorTest {
     private static final Character charZUppercase = 'Z';
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         comparator = new CustomCharacterComparator();
     }
 
     @Test
-    public void compareShouldReturnEqualRegardlessOfCase() {
-        assertThat(comparator.compare(charALowecase,charAUppercase),equalTo(0));
+    public void sameCharactersShouldBeEqualForSameCase() {
         assertThat(comparator.compare(charALowecase,charALowecase),equalTo(0));
         assertThat(comparator.compare(charAUppercase,charAUppercase),equalTo(0));
     }
 
+    @Test
+    public void sameCharactersShouldBeEqualForDifferentCase() {
+        assertThat(comparator.compare(charALowecase,charAUppercase),equalTo(0));
+    }
+
+    @Test
+    public void smallCharactersShouldBeLessThanLargeCharactersSameCase(){
+        assertThat(comparator.compare(charALowecase,charZLowecase),lessThan(0));
+        assertThat(comparator.compare(charAUppercase,charZUppercase),lessThan(0));
+    }
+
+    @Test
+    public void smallCharactersShouldBeLessThanLargeCharactersDifferentCase(){
+        assertThat(comparator.compare(charALowecase,charZUppercase),lessThan(0));
+    }
 
 }
