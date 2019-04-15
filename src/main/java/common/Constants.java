57226215b60c34fcd6168965181aa5eabcfa986f
Ayush Kohli <ayushk96@hotmail.com>
2019-04-14 22:09:12 -0500

    Changing port for travis

--- src/main/java/common/Constants.java
+++ src/main/java/common/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
   //constants used by server and client
   public static final String ADDRESS = "localhost";
-  public static final int PORT = 8080;
+  public static final int PORT = 4123;
 
   private Constants() {} //Forcing developer not to instantiate this class
 }
